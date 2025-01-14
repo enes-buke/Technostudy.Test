6ad3395ffe4bfeea789bc582590baf1d705380f3
Enes <enes.buke12@gmail.com>
2025-01-14 14:44:12 +0300

    Initial commit

---
+++ src/test/java/Utilities/BaseDriver.java
@@ -0,0 +1,37 @@
+package Utilities;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.time.Duration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class BaseDriver {
+
+    public static WebDriver driver;
+    public static WebDriverWait wait;
+
+    @BeforeClass
+    public void StartAnnotation() {
+
+        Logger logger = Logger.getLogger("");
+        logger.setLevel(Level.SEVERE);
+
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+    }
+
+    @AfterClass
+    public void FinishAnnotation() {
+        Func.Wait(5);
+        driver.quit();
+    }
+}
---
+++ src/test/java/Utilities/Func.java
@@ -0,0 +1,11 @@
+package Utilities;
+
+public class Func {
+    public static void Wait(int sn){
+        try {
+            Thread.sleep(sn*1000l);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
